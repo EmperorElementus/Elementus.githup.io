5322d7dc9ae67bd409d0ce04fba444ae43e4da2e
EmperorElementus <161367633+EmperorElementus@users.noreply.github.com>
2024-04-20 21:19:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#all-peppers",
+    data:{
+        products:[
+            {id:1,title:"Jalapeño",short_text:"Medium-sized Red Blunt Pepper",image:'../images/Jalapeño.jpg',desc:"Full desk"},
+            {id:2,title:"Serrano",short_text:"Small-sized Red Cylindrical Pepper",image:'../images/Serrano.jpg',desc:"Full desk"},
+            {id:3,title:"Cayenne",short_text:"Cayenne Long Red Slim Pepper",image:'../images/Cayenne Pepper.png',desc:"Full desk"},
+            {id:4,title:"Habanero",short_text:"Habanero Small-sized Orange Lantern Pepper",image:'../images/Habanero.png',desc:"Full desk"},
+            {id:5,title:"Tabasco",short_text:"Tabasco Small-sized Red Round Pepper",image:'../images/Tabasco.png',desc:"Full desk"},
+        ]
+    },
+});
